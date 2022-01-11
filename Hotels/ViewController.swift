476e52091b953fe90afdc039ee4442ecdf755a42
Dmitry Pavlov <perr07@yandex.ru>
2022-01-11 18:51:40 +0300

    Initial Commit

---
+++ Hotels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotels
+//
+//  Created by Pandos on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
